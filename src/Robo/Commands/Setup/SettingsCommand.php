e964c9e78c1a79f89100ed14311865f1efe2adf2
Dane Powell <git@danepowell.com>
2018-08-31 16:08:36 -0700

    testing

--- src/Robo/Commands/Setup/SettingsCommand.php
+++ src/Robo/Commands/Setup/SettingsCommand.php
@@ -75,6 +75,14 @@ class SettingsCommand extends BltTasks {
       $default_local_settings_file = "$multisite_dir/settings/default.local.settings.php";
       $project_local_settings_file = "$multisite_dir/settings/local.settings.php";
 
+      // Generate default.includes.settings.php.
+      $blt_includes_settings_file = $this->getConfigValue('blt.root') . '/settings/default.includes.settings.php';
+      $default_includes_settings_file = "$multisite_dir/settings/default.includes.settings.php";
+
+      // Generate sites/settings/global.settings.default.php.
+      $blt_glob_settings_file = $this->getConfigValue('blt.root') . '/settings/global.settings.default.php';
+      $default_glob_settings_file = $this->getConfigValue('docroot') . "/sites/settings/global.settings.default.php";
+
       // Generate local.drush.yml.
       $blt_local_drush_file = $this->getConfigValue('blt.root') . '/settings/default.local.drush.yml';
       $default_local_drush_file = "$multisite_dir/default.local.drush.yml";
@@ -83,6 +91,8 @@ class SettingsCommand extends BltTasks {
       $copy_map = [
         $blt_local_settings_file => $default_local_settings_file,
         $default_local_settings_file => $project_local_settings_file,
+        $blt_includes_settings_file => $default_includes_settings_file,
+        $blt_glob_settings_file => $default_glob_settings_file,
         $blt_local_drush_file => $default_local_drush_file,
         $default_local_drush_file => $project_local_drush_file,
       ];
